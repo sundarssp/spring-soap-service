e4c5ebf39aff9e6f6bb951f87665b123c01f3f1d
Sundar Sethu <sundar.ssp@gmail.com>
2016-09-17 20:24:52 -0700

    Added decryption support on the server side

--- src/main/java/com/example/springintro/service/WebServiceConfig.java
+++ src/main/java/com/example/springintro/service/WebServiceConfig.java
@@ -12,6 +12,7 @@ import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.server.EndpointInterceptor;
 import org.springframework.ws.server.endpoint.interceptor.PayloadLoggingInterceptor;
 import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+import org.springframework.ws.soap.security.wss4j2.callback.KeyStoreCallbackHandler;
 import org.springframework.ws.soap.security.wss4j2.support.CryptoFactoryBean;
 import org.springframework.ws.soap.server.endpoint.interceptor.SoapEnvelopeLoggingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
@@ -29,20 +30,36 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 		interceptors.add(new SoapEnvelopeLoggingInterceptor());
 
 		Wss4jSecurityInterceptor security = new Wss4jSecurityInterceptor();
-		security.setValidationActions("Signature");
+		security.setValidationActions("Encrypt");
 
-		addSignatureValidation(security, interceptors);
+		addEncryptionValidation(security, interceptors);
+
+		interceptors.add(security);
 	}
 
-	private void addSignatureValidation(Wss4jSecurityInterceptor security, List<EndpointInterceptor> interceptors) {
+	/**
+	 * Encryption validatin (Decryption) requires private key store's password.
+	 * Spring cannot validate if the complete SOAP body is encrypted. In SOAP UI make sure the 'parts' is selected for the 
+	 * encryption. Set the parts to "ID: <empty>", Name: getPopulationRequest, Namespace: http://www.example.com/springintro/population
+	 * Encode: content.
+	 * 
+	 * @param security
+	 * @param interceptors
+	 */
+	private void addEncryptionValidation(Wss4jSecurityInterceptor security, List<EndpointInterceptor> interceptors) {
 		CryptoFactoryBean cfb = new CryptoFactoryBean();
 		try {
-			cfb.setKeyStoreLocation(new ClassPathResource("consumerpublicstore.jks"));
+			cfb.setKeyStoreLocation(new ClassPathResource("serverprivatestore.jks"));
 			cfb.setKeyStorePassword("keyStorePassword");
 			cfb.afterPropertiesSet();
 
-			security.setValidationSignatureCrypto(cfb.getObject());
-			interceptors.add(security);
+			security.setValidationDecryptionCrypto(cfb.getObject());
+
+			KeyStoreCallbackHandler ksch = new KeyStoreCallbackHandler();
+			ksch.setPrivateKeyPassword("myaliasPassword");
+
+			security.setValidationCallbackHandler(ksch);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
